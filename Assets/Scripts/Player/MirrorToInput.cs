e51bc2d007dfda9207c6b57069f68378649ae96e
Faragó Balázs <minimalgeek@gmail.com>
2017-03-13 22:31:01 +0100

    Sprite mirroring, trail

---
+++ Assets/Scripts/Player/MirrorToInput.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MirrorToInput : MonoBehaviour {
+
+	void Update () {
+		float horizontal = Input.GetAxisRaw("Horizontal");
+		if (horizontal < 0) {
+			transform.localScale = new Vector3(-1,1,1);
+		} else if (horizontal > 0) {
+			transform.localScale = new Vector3(1,1,1);
+		}
+	}
+}
