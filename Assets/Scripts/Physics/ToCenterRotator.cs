a0e1b50de4862a64c403094c32f5cdc858beaca8
Faragó Balázs <minimalgeek@gmail.com>
2017-03-17 22:28:34 +0100

    Remove gravity refernce

--- Assets/Scripts/Physics/ToCenterRotator.cs
+++ Assets/Scripts/Physics/ToCenterRotator.cs
@@ -3,18 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 using Gamelogic.Extensions;
 
+[ExecuteInEditMode]
 public class ToCenterRotator : GLMonoBehaviour
 {
 
-    private Gravity worldCenter;
-    void Start()
-    {
-        worldCenter = FindObjectOfType(typeof(Gravity)) as Gravity;
-    }
-
     void Update()
     {
-        Vector3 vectorToTarget = worldCenter.transform.position - transform.position;
+        Vector3 vectorToTarget = -transform.position;
         float angle = Mathf.Atan2(vectorToTarget.y, vectorToTarget.x) * Mathf.Rad2Deg;
         this.transform.rotation = Quaternion.Euler(0,0,angle - 90);
     }
