a0256b60c01cc4cb02c70336b058be786c1d4e08
Faragó Balázs <minimalgeek@gmail.com>
2017-03-17 15:50:01 +0100

    Actions - base

---
+++ Assets/Scripts/Player/Actions/BaseAction.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Gamelogic.Extensions;
+using System;
+
+public class BaseAction : GLMonoBehaviour, IAction {
+    
+    void Update () {
+		if (Input.GetKeyUp(KeyCode.LeftControl) || Input.GetKeyUp(KeyCode.RightControl)) {
+			Execute();
+		}
+	}
+
+	public virtual void Execute()
+    {
+    }
+}
---
+++ Assets/Scripts/Player/Actions/SwitchGravityFieldRotationSpeedAction.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+public class SwitchGravityFieldRotationSpeedAction : BaseAction {
+
+	private int currentIndex;
+	public float[] states;
+
+	void Awake()
+	{
+		Assert.IsNotNull(states);
+		Assert.IsTrue(states.Length > 1, "Provide at least 2 states");
+	}
+	
+	void Start()
+	{
+		
+	}
+
+	public override void Execute() {
+
+	}
+
+}
