8bae8a57c07c0e035799e920ab6865c190955bc5
viktorn <nagyviktor2@gmail.com>
2017-03-23 11:07:13 +0100

    Camera fix (zoom)

--- Assets/Scripts/Other/CameraZoom.cs
+++ Assets/Scripts/Other/CameraZoom.cs
@@ -3,32 +3,24 @@ using UnityEngine;
 using Gamelogic.Extensions;
 
 [RequireComponent(typeof(Camera))]
-public class Camera2DFollowFix : GLMonoBehaviour
+public class CameraZoom : GLMonoBehaviour
 {
-    public Transform target;
     public float yOffset = 0.3f;
     
-    private float zOffset;
     private Camera cam;
     private float zoomMultiplier = 1.3f;
 
     // Use this for initialization
     private void Start()
     {
-        if (target == null)
-        {
-            target = GameObject.FindGameObjectWithTag(TagsAndLayers.PLAYER).transform;
-        }
         cam = GetComponent<Camera>();
-        zOffset = (transform.position - target.position).z;
-        transform.parent = null;
     }
 
 
     // Update is called once per frame
     private void Update()
     {
-        if (!target || !cam)
+        if ( !cam)
         {
             return;
         }
@@ -38,8 +30,7 @@ public class Camera2DFollowFix : GLMonoBehaviour
         {
             cam.orthographicSize *= Mathf.Pow(zoomMultiplier, -zoomDelta);
         }
-
-        transform.rotation = target.transform.rotation;
-        transform.localPosition = new Vector3(target.position.x, target.position.y, zOffset) + target.transform.up * yOffset * cam.orthographicSize;
+        
+        transform.localPosition = Vector3.up * yOffset * cam.orthographicSize + Vector3.zero.WithZ(transform.localPosition.z);
     }
 }
