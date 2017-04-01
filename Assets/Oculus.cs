9ed774d1a91f2e3612aee6b4dea53f6f81cbd658
viktorn <nagyviktor2@gmail.com>
2017-04-02 00:01:16 +0200

    Oculus support

---
+++ Assets/Oculus.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Oculus : MonoBehaviour {
+
+	Rigidbody2D parentRB;
+	CombinedController player;
+
+	// Use this for initialization
+	void Start () {
+		parentRB = GetComponentInParent<Rigidbody2D>();
+		player = GetComponentInParent<CombinedController>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(Vector3.forward,(Vector3)parentRB.velocity + player.GetFacingVector()), 0.1f);
+	}
+}
