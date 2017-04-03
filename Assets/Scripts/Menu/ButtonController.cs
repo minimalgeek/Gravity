7b17bb087b92854c69011054aa9679e113e5296f
Faragó Balázs <minimalgeek@gmail.com>
2017-04-04 01:09:38 +0200

    Level loader

---
+++ Assets/Scripts/Menu/ButtonController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class ButtonController : MonoBehaviour
+{
+    private Button myButton;
+	public string sceneNameToStart;
+
+    void Awake()
+    {
+        myButton = GetComponent<Button>();
+        myButton.onClick.AddListener(() => { SceneManager.LoadScene(sceneNameToStart); });
+    }
+
+}
